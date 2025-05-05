1ccff046df6c93298011d6e945c8fe5a09d70d6f
ElenaFaur <faurelena2000@yahoo.com>
2025-05-05 23:48:54 +0300

    finished the whole map design

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rigidbody2D player_rb;
+    private Rigidbody2D rb;
+    public float speed = 7f;
 
     // Start is called before the first frame update
     private void Start()
     {
-        player_rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxisRaw("Horizontal");
-        player_rb.velocity = new Vector2(dirX * 7f, player_rb.velocity.y);
+        rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
 
         if(Input.GetButtonDown("Jump"))
         {
-            player_rb.velocity = new Vector2(player_rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, 14f);
         }
     }
 }
